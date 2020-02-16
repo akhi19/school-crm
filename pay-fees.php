82b0aecaae673305c482cde1505c0e99b7b86e9b
akhi19 <32562479+akhi19@users.noreply.github.com>
2020-02-16 12:30:55 +0530

    Add files via upload

--- pay-fees.php
+++ pay-fees.php
@@ -8,37 +8,8 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
 		<h1>
-                <?php
-					$empty = "Student not registered for online fees payment. Contact school administrator";
-					$emptyClass = "Fees not available for this class";
+                <?php echo $_POST["first_name"]." ". $_POST["last_name"]; 
                     $allFees = getPendingFeesByGrNo($_POST["gr_no"]);
-					if (!($allFees)){
-					?>
-						<div class="alert alert-danger">
-                                    <strong>Error!</strong> <?php echo $empty; return;?>
-                        </div>
-					<?php }
-					$i = 0 ;
-					foreach ($allFees as $row)
-					{ 
-						if ($row['academic_year'] == 0){
-							$fees[$i] = 0;
-							$i++;
-							continue;
-						}
-						$classYearwiseFees[$i] = getClassYearwiseFees($allFees[$i]['class'],$allFees[$i]['academic_year']);
-						if (!($classYearwiseFees[$i])){
-						?>
-							<div class="alert alert-danger">
-                                    <strong>Error!</strong> <?php echo $emptyClass; return;?>
-                    	    </div>
-				
-						<?php }
-						$fees[$i] = $classYearwiseFees[$i]["total_fees"];
-						$i++;
-					}
-					echo $_POST["first_name"]." ". $_POST["last_name"];
-					$monthsMapper = ["Jun","Jul","Aug","Sep","Oct","Nov","Dec","Jan","Feb", "Mar", "Apr", "May"];
                 ?>
             </h1>
             <ol class="breadcrumb">
@@ -47,6 +18,23 @@
 
             </ol>
         </section>
+        <?php 
+            $totalFeesToPay = 0;
+            $monthsMapper = [
+                "1" => "Jan",
+                "2" => "Feb",
+                "3" => "Mar",
+                "4" => "Apr",
+                "5" => "May",
+                "6" => "Jun",
+                "7" => "Jul",
+                "8" => "Aug",
+                "9" => "Sep",
+                "10" => "Oct",
+                "11" => "Nov",
+                "0" => "Dec"
+            ];
+        ?>
         <!-- Main content -->
         <section class="content">
             <div class="row">
@@ -58,23 +46,61 @@
                             <div class="col-md-5">
                                 <div class="form-group">
                                     <label>Pay Fees for </label>
-                                    <input name="pay_for_months_number" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);
-                                                                    if (this.value > 2) this.value = 2" type="number" maxlength="2" style="width: 4.5rem; height: 2.5rem" />
+                                    <input id="pay_for_months_number" name="pay_for_months_number" oninput="javascript: 
+                                        if (this.value.length > this.maxLength)
+                                             this.value = this.value.slice(0, this.maxLength);
+                                        if (this.value > 2) 
+                                            this.value = 2; 
+                                        if (this.value < 0)
+                                            this.value = 0;
+                                        var feesToPay = 0;
+                                        for(let i=0; i<this.value; i++) {
+                                            var fees = $('#fees_info_table').find('tbody tr:nth-child('+ parseInt(i+2) +') td:nth-child(5)').text();
+                                            feesToPay += parseInt(fees);
+                                        }
+                                        $('#payableAmountValue').text(feesToPay);
+                                            " 
+                                     type="number" maxlength="2" style="width: 4.5rem; height: 2.5rem" />
                                     <label> month(s) </label>
                                 </div>
                             </div>
                         </div>
-
+                        <div id="message-response"> </div>
                             <div class="row">
                             <?php if(in_array($_SESSION['sessUser'], array('admin'))){ ?>
-                                        <a href="javascript:void(0)" class="btn btn-primary" data-toggle="modal" data-target="#paymentMethod"> Pay </a>
-                                    <?php } else { ?>
                                 <div class="col-md-5">
+                                    <div class="form-group" id="payment_mode">
+                                        <label for="text">Payment Mode:</label>
+                                        <label class="radio-inline">
+                                            <input type="radio" value="Cash"  name="payment_mode_option">Cash
+                                        </label>
+                                        <label class="radio-inline">
+                                            <input type="radio" value="Debit Card" name="payment_mode_option">Debit Card
+                                        </label>
+                                        <label class="radio-inline">
+                                            <input type="radio" value="Credit Card" name="payment_mode_option"> Credit Card
+                                        </label>
+                                    </div>
                                     <div class="form-group">
-                                            <button type="button"  onclick="" class="btn btn-primary btn-flat submit-trigger">PayNow</button>
+                                        <label>Payable Amount: </label>
+                                        <label id="payableAmountValue">  </label>
+                                     </div>
+                                    <div class="form-group">
+                                            <button type="button"  onclick="getMultipleColumnValue();" class="btn btn-primary
+                                            btn-flat submit-trigger">Pay Now</button>
                                             <button type="button" style="display: none;"   class="btn btn-primary  btn-flat submit-process"><i class="fa fa-spinner fa-spin" ></i> Process...</button>
                                     </div>
-                                </div>
+                                        
+                                 </div>
+                            <?php } else { ?>
+                                <!-- <div class="col-md-5">
+                                    <div class="form-group">
+                                            <button type="button"  onclick="getMultipleColumnValue();" class="btn btn-primary
+                                            btn-flat submit-trigger">Pay Now</button>
+                                            <button type="button" style="display: none;"   class="btn btn-primary  btn-flat submit-process"><i class="fa fa-spinner fa-spin" ></i> Process...</button>
+                                    </div>
+                                </div> -->
+
                                     <?php }?>
                             </div>
                         </div>
@@ -83,7 +109,7 @@
                         <div class="box-body">
 
                             <div id="error-student"></div>
-                                <table  class="table table-bordered table-striped">
+                                <table  id = "fees_info_table" class="table table-bordered table-striped">
                                     <thead>
                                     <tr>
                                         <th>Class</th>
@@ -102,25 +128,19 @@
                                             <td></td>
                                         </tr>
                                         <?php if(!empty($allFees)) { ?>
-									<?php $i = 0; foreach($allFees as $row) {; ?>
+									<?php $i = 0; foreach($allFees as $row) { $i++; ?>
                                                 <?php 
-													 $months = $row['fees_months'];
-													 if ($row['academic_year'] == 0){
-													 	$i++;
-													 	continue;
-													 }
-                                                     while($months <= 11){ ?>
+                                                     while($monthsMapper[$row['fees_months']%12] != "Jun") { ?>
                                                          <tr>
                                                             <td><?php echo $row['class']; ?> </td>
 											                <td><?php echo $row['academic_year']; ?></td>
-                                                            <td><?php echo $monthsMapper[$months]; ?></td>
+                                                            <td><?php echo $monthsMapper[$row['fees_months']%12]; ?></td>
                                                             <td></td>
-                                                            <td> <?php echo $fees[$i]; ?> </td>
+                                                            <td> 200 <?php $totalFeesToPay += 200; ?> </td>
+                                                            <td style="display:none;"> <?php echo $row['gr_no']; ?></td>
                                                          </tr>
-                                                        <?php 
-														$months++;
+                                                        <?php $row['fees_months']++;
                                                     }
-													$i++;
                                                 ?>
 									<?php } ?>
 								<?php } ?>
@@ -136,22 +156,4 @@
         </section>
 <?php } ?>
 <!-- ./wrapper -->
-<div id="paymentMethod" class="modal fade" role="dialog">
-        <div class="modal-dialog modal-lg">
-
-            <!-- Modal content-->
-            <div class="modal-content">
-                <div class="modal-header">
-                    <!-- <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Add Fees</h4> -->
-                </div>
-                <div class="modal-body">
-                    <div id="response">agsjhasgdjhg</div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-</div>
 <?php require_once 'footer.php'; ?>
